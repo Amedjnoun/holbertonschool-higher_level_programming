a67708f846f2a3dbcb8e3810c3444ecd9c2027a8
Ali <a.medjnoun@gmail.com>
2025-01-16 19:45:11 +0100

    6 7 8 9

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str = "Holberton School"
-str = str[0:9]
-print(str * 3)
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = str[39:67] + str[107:112] + str[0:6]
 print(str)
--- python-hello_world/9-easter_egg.py
+++ python-hello_world/9-easter_egg.py
@@ -1,2 +1,4 @@
 #!/usr/bin/python3
 import this
+this.s = ""
+print(this.s)
