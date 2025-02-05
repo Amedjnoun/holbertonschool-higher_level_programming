0d02daf03267ef9835e0a1ec8c2c3ba9d2f354dd
Ali <a.medjnoun@gmail.com>
2025-02-05 21:13:23 +0100

    pycode

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
-
-
 def lookup(obj):
-	"""
-	lookup Returns the list of available attributes and methods of an object
+    """
+    lookup Returns the list of available attributes and methods of an object
 
-	args:
-		obj: object to be inspected
+    args:
+        obj: object to be inspected
 
-	returns:
-		lookup_list: list of available attributes and methods of an object
-	"""
-	lookup = dir(obj)
-	return lookup
+    returns:
+        lookup_list: list of available attributes and methods of an object
+    """
+    lookup = dir(obj)
+    return lookup
