8b1fa916571af28d38f931ba58556b9814ac76bf
Ali <a.medjnoun@gmail.com>
2025-02-14 17:25:09 +0100

    pycodestyle

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -5,6 +5,7 @@ Module that defines the save_to_json_file function.
 
 import json
 
+
 def save_to_json_file(my_obj, filename):
     """
     Writes an Object to a text file, using a JSON representation.
