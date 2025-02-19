7d2cfa7e716dd466e96100c6f8aa8345d3e65706
Ali <a.medjnoun@gmail.com>
2025-02-19 20:15:45 +0100

    task 4 comments added

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -37,7 +37,7 @@ def get_user(username):
     if user:
         return jsonify(user)
     else:
-        return jsonify({"error": "User not found"}), 404
+        return jsonify({"error": "User not found"}), 400
 
 
 @app.route('/add_user', methods=['POST'])
