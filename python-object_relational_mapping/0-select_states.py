2559249bf458b9926d2051c9f0d3616a3a2dade6
Ali <a.medjnoun@gmail.com>
2025-03-09 21:44:10 +0100

    task 0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
 """
-Script that lists all states from the database hbtn_0e_0_usa.
+Module that lists all states from database hbtn_0e_0_usa.
+
+After connecting to MySQL server running on localhost
+at port 3306 to access database hbtn_0e_0_usa,
+it returns all states from database sorted in ascending order by states.id.
 """
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
+    # Connect to the MySQL database
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
@@ -13,10 +18,20 @@ if __name__ == "__main__":
         passwd=sys.argv[2],
         db=sys.argv[3]
     )
+
+    # Create a cursor object
     cur = db.cursor()
+
+    # Execute the SQL query
     cur.execute("SELECT * FROM states ORDER BY id ASC")
+
+    # Fetch all the rows
     rows = cur.fetchall()
+
+    # Print each row
     for row in rows:
         print(row)
+
+    # Close cursor and database connection
     cur.close()
     db.close()
