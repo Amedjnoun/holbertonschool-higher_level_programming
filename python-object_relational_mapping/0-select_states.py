ada56a99d9a53cb24d3a61f81d982976f714b3c0
Ali <a.medjnoun@gmail.com>
2025-03-09 21:45:14 +0100

    task 0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,37 +1,30 @@
-#!/usr/bin/python3
-"""
-Module that lists all states from database hbtn_0e_0_usa.
-
-After connecting to MySQL server running on localhost
-at port 3306 to access database hbtn_0e_0_usa,
-it returns all states from database sorted in ascending order by states.id.
-"""
 import MySQLdb
-import sys
+from sys import argv
 
-if __name__ == "__main__":
-    # Connect to the MySQL database
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3]
+
+def connect_database(username, password, db_name):
+    """
+    Args:
+        username (str): mysql username
+        password (str): mysql password
+        database_name (str): database name
+
+    Returns:
+        Returns MySQL connection to database
+    """
+    return MySQLdb.connect(
+        host="localhost", port=3306, user=username, passwd=password, db=db_name
     )
 
-    # Create a cursor object
-    cur = db.cursor()
 
-    # Execute the SQL query
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
+if __name__ == "__main__":
+    """Main entry point"""
 
-    # Fetch all the rows
-    rows = cur.fetchall()
+    with connect_database(argv[1], argv[2], argv[3]) as db:
 
-    # Print each row
-    for row in rows:
-        print(row)
+        # ensuring query executes well  by using cursor object
+        cursor = db.cursor()
+        cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
 
-    # Close cursor and database connection
-    cur.close()
-    db.close()
+        for state in cursor.fetchall():
+            print(state)
