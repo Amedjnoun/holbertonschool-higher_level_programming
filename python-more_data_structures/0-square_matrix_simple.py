72c01f47f9a5070e8176664226281bf12c5d00d7
Ali <a.medjnoun@gmail.com>
2025-03-21 14:58:38 +0100

    Flake8 and LF

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,2 +1,7 @@
+#!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    return [[x ** 2 for x in row] for row in matrix]
+    """
+    Computes the square value of all integers of a matrix.
+    Returns a new matrix of the same size with squared values.
+    """
+    return [[x**2 for x in row] for row in matrix]
--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,3 +1,7 @@
+#!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
+    """
+    Prints a dictionary by ordered keys.
+    """
     for key in sorted(a_dictionary.keys()):
-        print(f"{key}: {a_dictionary[key]}")
+        print("{}: {}".format(key, a_dictionary[key]))
